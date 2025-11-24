b76d770dce3f681df4095b70d4d7b110d0702d55
Marco Alzamora Cueto <alzamora1008@gmail.com>
2025-11-24 11:12:51 -0500

    oi commit

--- src/app/features/card-detail/card-detail.component.ts
+++ src/app/features/card-detail/card-detail.component.ts
@@ -23,7 +23,8 @@ export class CardDetailComponent implements OnInit {
   sellers = computed(() => [
     { name: 'Payasito store', stock: 3, price: 14.50 },
     { name: 'Naranja', stock: 1, price: 16.00 },
-    { name: 'Pachacutec city', stock: 5, price: 13.00 }
+    { name: 'Pachacutec city', stock: 5, price: 13.00 },
+    { name: 'Caballito de Totora Store', stock: 5, price: 149.00 },
   ]);
 
   ngOnInit() {
